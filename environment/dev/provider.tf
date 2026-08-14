40dedc62bd43433fc6708b036cc1e0b37141164b
Sujay <chaurasia.sujay@gmail.com>
2026-08-14 22:25:08 +0530

    backend added

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "backend_sujay"
-    storage_account_name = "sujay1tfstatefile"      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    resource_group_name  = "backend_bala"
+    storage_account_name = "bala1tfstatefile"      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
