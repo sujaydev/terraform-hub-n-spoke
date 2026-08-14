7736bd0d62336468d9778a181a0087f9ba09ec12
Sujay <chaurasia.sujay@gmail.com>
2026-08-14 14:39:41 +0530

    added backedn

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=5.0.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "backend_sujay"
+    storage_account_name = "sujay1tfstatefile"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 provider "azurerm" {
